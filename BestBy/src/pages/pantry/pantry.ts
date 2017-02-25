2d32aa97113496f4faed21ca581bcb259b4f46a9
yehkatie <katieyeh@BigMac-Pro.local>
2017-02-25 03:10:40 -0800

    User sees added item into pantry
    - still need to work on design… (show expiration date)

--- BestBy/src/pages/pantry/pantry.ts
+++ BestBy/src/pages/pantry/pantry.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods} from 'angularfire2';
+import { AlertController } from 'ionic-angular';
 import { NavController, NavParams } from 'ionic-angular';
+import { FIREBASE_PROVIDERS, defaultFirebase,FirebaseListObservable } from 'angularfire2';
+import firebase from 'firebase';
 import { AddItemPage } from '../add-item/add-item';
 
 /*
@@ -14,18 +18,56 @@ import { AddItemPage } from '../add-item/add-item';
 })
 export class PantryPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  userID: any;
+  //pantryItems: FirebaseListObservable<any>;
+  pantryList: string[] = [];
+  pantryItems: string[] = [];
+
+  constructor(public navCtrl: NavController, public af: AngularFire, public alertCtrl: AlertController) {
+
+    this.userID = this.af.auth.getAuth().uid;
+    //this.pantryItems = this.af.database.list('/Users/'+this.userID);
+    //console.log(this.pantryItems);
+
+    /*
+    af.database.list('Users/'+this.userID, { preserveSnapshot: true })
+      .subscribe(snapshots=> {
+        snapshots.forEach(snapshot=> {
+          this.pantryList.push(snapshot.val());
+        });
+      });
+      */
+
+    this.pantryItems = [];
+    af.database.list('/Users/'+this.userID, { preserveSnapshot: true })
+      .subscribe(snapshots => {
+        snapshots.forEach(snapshot => {
+          this.pantryItems.push(snapshot.key);
+        });
+      });
+
+    console.log(this.pantryItems);
+
+
+    /*
+    var ref = firebase.database().ref('Users/'+this.userID)
+    ref.on("value", function(snap) {
+      console.log(snap.numChildren());
+
+      console.log(snap.val());
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    }); */
+
+
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PantryPage');
   }
 
   public toAddItem(){
-
     this.navCtrl.push(AddItemPage);
-
-
   }
-
 
 }
